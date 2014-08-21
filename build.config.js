b37fd1e3f976c5f2c56a3c6458efba31c47709da
jforaker <jake@chalkable.com>
2014-08-21 19:43:55 -0400

    fixing injectify

--- build.config.js
+++ build.config.js
@@ -67,7 +67,12 @@ module.exports = {
 			'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
 			'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
 			'vendor/angular-ui-router/release/angular-ui-router.js',
-			'vendor/angular-ui-utils/modules/route/route.js'
+			'vendor/angular-ui-utils/modules/route/route.js',
+			'vendor/angular-resource/angular-resource.min.js',
+			'vendor/angular-animate/angular-animate.min.js',
+			'vendor/angular-route/angular-route.min.js',
+			'vendor/angular-localStorageService/src/storageprovider.js',
+			'vendor/angular-flash-messages/angular-flash.js'
 		],
 		css: [
 		],
